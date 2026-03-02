006da3bd409afd13abd7a17458d50e1a989704ed
Guilherme Duarte <guilhermeduartegd2005@gmail.com>
2026-03-02 15:35:46 -0300

    primeiro commit

---
+++ src/main/java/br/com/fatec/fatecrooms/FatecRoomsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.fatecrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatecRoomsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatecRoomsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fatec/fatecrooms/FatecRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.fatecrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatecRoomsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
