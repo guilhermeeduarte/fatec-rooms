a7e3f438a6cfe71dbbb2ad23feabadcc319a5215
Henrique Ribeiro <henriqueglribeiro@gmail.com>
2026-04-06 19:23:47 -0300

    Correções.

---
+++ src/main/java/br/com/fatec/fatecrooms/controller/UserController.java
@@ -0,0 +1,34 @@
+package br.com.fatec.fatecrooms.controller;
+
+import br.com.fatec.fatecrooms.DTO.UserSummaryDTO;
+import br.com.fatec.fatecrooms.model.User;
+import br.com.fatec.fatecrooms.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserSummaryDTO> me(Authentication authentication) {
+        String username = authentication.getName();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+
+        return ResponseEntity.ok(new UserSummaryDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getEmail(),
+                user.getAuthlevel(),
+                user.getEnabled()
+        ));
+    }
+}
