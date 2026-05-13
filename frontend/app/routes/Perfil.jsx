fcb3fe8844199e29e519b64faa6250532ae0a54b
Gabidanety <gabinete878@gmail.com>
2026-05-13 10:40:57 -0300

    subindo funcionalidades de acessibilidade

--- frontend/app/routes/Perfil.jsx
+++ frontend/app/routes/Perfil.jsx
@@ -13,6 +13,36 @@ export default function UserProfile() {
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
 
+  //acess
+  const [fontSize, setFontSize] = useState(
+    Number(localStorage.getItem("fontSize")) || 14
+  );
+
+  const [daltonicMode, setDaltonicMode] = useState(
+    localStorage.getItem("daltonicMode") === "true"
+  );
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--user-font-size",
+      `${fontSize}px`
+    );
+
+    localStorage.setItem("fontSize", fontSize);
+  }, [fontSize]);
+
+  useEffect(() => {
+    if (daltonicMode) {
+      document.body.classList.add("daltonic-mode");
+    } else {
+      document.body.classList.remove("daltonic-mode");
+    }
+
+    localStorage.setItem("daltonicMode", daltonicMode);
+  }, [daltonicMode]);
+
+  //
+
   const [user, setUser] = useState({
     firstname: "",
     lastname: "",
@@ -169,6 +199,40 @@ export default function UserProfile() {
       <div className="user-profile">
         {error && <div className="error-message">{error}</div>}
         {success && <div className="success-message">{success}</div>}
+
+        {/*ACESS*/}
+        <div className="accessibility-card">
+          <h3>Acessibilidade</h3>
+
+          <div className="accessibility-option">
+            <label>Modo para Daltonismo</label>
+
+            <button
+              type="button"
+              className={`btn-accessibility ${daltonicMode ? "active" : ""}`}
+              onClick={() => setDaltonicMode(!daltonicMode)}
+            >
+              {daltonicMode ? "Ativado" : "Desativado"}
+            </button>
+          </div>
+
+          <div className="accessibility-option">
+            <label>
+              Tamanho da Fonte: <strong>{fontSize}px</strong>
+            </label>
+
+            <input
+              type="range"
+              min="14"
+              max="20"
+              step="1"
+              value={fontSize}
+              onChange={(e) => setFontSize(Number(e.target.value))}
+              className="font-slider"
+            />
+          </div>
+        </div>
+        {/* --- */}
 
         {!editando ? (
           <div className="user-card">
